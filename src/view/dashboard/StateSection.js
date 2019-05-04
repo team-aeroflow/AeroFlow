847d105ad21c7970ba99de89fce3ba740274ea0c
Peerasorn Hemsart <peerasorn@Peerasorns-MacBook-Pro.local>
2019-05-04 18:19:24 +0700

    create add state button

--- src/view/dashboard/StateSection.js
+++ src/view/dashboard/StateSection.js
@@ -2,24 +2,42 @@ import React from 'react'
 import './StateSection.css'
 
 class StateSection extends React.Component {
+  
   render() {
     const { countMeta } = this.props
-    // console.log(countMeta)
+
     return (
       <div className="state-section">
-        {
-          Object.keys(countMeta).map((key, i) => {
-            return (
-              <div key={i}>
-                <h3>State : {key}</h3>
-                <p>Actions {countMeta[key].actions}</p>
-                <p>Channels {countMeta[key].channels}</p>
-                <p>Effects {countMeta[key].effects}</p>
-                <p>Reducers {countMeta[key].reducers}</p>
-              </div>
-            )
-          })
-        }
+        <div className="state-info">
+          {
+            Object.keys(countMeta).map((key, i) => {
+              return (
+                <div key={i} className="state-block">
+                  <h3>State : {key}</h3>
+                  <p>
+                    <span className="dot" style={{ backgroundColor: 'red' }}></span>
+                    Actions {countMeta[key].actions}
+                  </p>
+                  <p>
+                    <span className="dot" style={{ backgroundColor: 'yellow' }}></span>
+                    Channels {countMeta[key].channels}
+                  </p>
+                  <p>
+                    <span className="dot" style={{ backgroundColor: 'green' }}></span>
+                    Effects {countMeta[key].effects}
+                  </p>
+                  <p>
+                    <span className="dot" style={{ backgroundColor: 'blue' }}></span>
+                    Reducers {countMeta[key].reducers}
+                  </p>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="add-state-block">
+          <div id="add-state"> + Add States</div>
+        </div>
       </div>
     )
   }
